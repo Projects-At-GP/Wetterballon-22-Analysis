38ada4f8d9437057082e9324e9fbf1e996a4a725
AlbertUnruh <keenan.noack@gmail.com>
2022-01-20 20:14:49 +0100

    more 💩 code

--- Wetterballon_22_Analysis/data.py
+++ Wetterballon_22_Analysis/data.py
@@ -5,5 +5,21 @@ def str_to_dict(
     text: str,
     /,
     *keys: str,
+    sep: str = ",",
 ) -> dict[str, str]:
-    ...
+    """
+    Parameters
+    ----------
+    text: str
+        The text which should be split into multiple values.
+    keys: str
+        The keys for the values.
+    sep: str
+        The separator for the values.
+
+    Returns
+    -------
+    dict[str, str]
+    """
+    values = text.split(sep)
+    return {key: value for key, value in zip(keys, values)}
--- Wetterballon_22_Analysis/visual.py
+++ Wetterballon_22_Analysis/visual.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 from io import TextIOWrapper
+from .data import str_to_dict
 
 
 __all__ = ("plot",)
@@ -9,4 +10,7 @@ __all__ = ("plot",)
 def plot(
     file: TextIOWrapper,
 ) -> None:
-    print(f"{file.name} was plotted...")
+    with file as f:
+        keys = f.readline().removesuffix("\n").split(",")
+        for _ in range(10):
+            print(str_to_dict(f.readline().removesuffix("\n"), *keys))
