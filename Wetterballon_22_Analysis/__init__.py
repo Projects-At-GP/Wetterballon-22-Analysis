ab078a644a3ab9856bce0e03436033bf588dec75
AlbertUnruh <keenan.noack@gmail.com>
2022-01-24 18:12:37 +0100

    update `__init__.py`

--- Wetterballon_22_Analysis/__init__.py
+++ Wetterballon_22_Analysis/__init__.py
@@ -1,2 +1,8 @@
+__author__ = "AlbertUnruh"
+__url__ = "https://github.com/Projects-At-GP/Wetterballon-22-Analysis"
+__license__ = "MIT"
+__copyright__ = f"(c) {__author__}"
+__description__ = "Code for the data analysis for out project."
+
 from .data import *
 from .visual import *
