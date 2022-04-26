2c31e2a1d0dc31f57fb9ffd7ee5e5fc964d533d3
AlbertUnruh <AlbertUnruh@pm.me>
2022-04-26 08:53:11 +0200

    dump everything

--- Wetterballon_22_Analysis/utils.py
+++ Wetterballon_22_Analysis/utils.py
@@ -54,6 +54,8 @@ def print_args(**kwargs) -> None:
 
 
 def prepare_csv_from_STRATOFLIGHTS_LOG(path: str) -> None:
+    p1 = "{},{}"
+    p2 = "{},{},{}"
     Path("data").mkdir(exist_ok=True)
     with (
         open(Path(path)) as f,
@@ -61,21 +63,56 @@ def prepare_csv_from_STRATOFLIGHTS_LOG(path: str) -> None:
         open(Path("data/1_lat.csv"), "w") as f1_lat,
         open(Path("data/1_lon.csv"), "w") as f1_lon,
         open(Path("data/2_lat_lon.csv"), "w") as f2_lat_lon,
-        open(Path("data/1_speed.csv"), "w") as f1_speed,
+        open(Path("data/1_speed_kmh.csv"), "w") as f1_speed_kmh,
         open(Path("data/1_course.csv"), "w") as f1_course,
-        open(Path("data/1_altitude.csv"), "w") as f1_altitude,
+        open(Path("data/1_altitude_nn.csv"), "w") as f1_altitude_nn,
         open(Path("data/1_temp_board.csv"), "w") as f1_temp_board,
         open(Path("data/1_temp_ext.csv"), "w") as f1_temp_ext,
         open(Path("data/2_temp_board_ext.csv"), "w") as f2_temp_board_ext,
         open(Path("data/1_hum_board.csv"), "w") as f1_hum_board,
         open(Path("data/1_hum_ext.csv"), "w") as f1_hum_ext,
         open(Path("data/2_hum_board_ext.csv"), "w") as f2_hum_board_ext,
-        open(Path("data/1_press.csv"), "w") as f1_press,
+        open(Path("data/1_press_hpa.csv"), "w") as f1_press_hpa,
         open(Path("data/2_altitude_press.csv"), "w") as f2_altitude_press,
     ):
         print(f.readline(), end="")
+
+        print("up_time,stats_in_use", file=f1_sats_in_use)
+        print("up_time,lat", file=f1_lat)
+        print("up_time,lon", file=f1_lon)
+        print("up_time,lat,lon", file=f2_lat_lon)
+        print("up_time,speed_kmh", file=f1_speed_kmh)
+        print("up_time,course", file=f1_course)
+        print("up_time,altitude_nn", file=f1_altitude_nn)
+        print("up_time,temp_board", file=f1_temp_board)
+        print("up_time,temp_ext", file=f1_temp_ext)
+        print("up_time,temp_board,temp_ext", file=f2_temp_board_ext)
+        print("up_time,hum_board", file=f1_hum_board)
+        print("up_time,hum_ext", file=f1_hum_ext)
+        print("up_time,hum_board,hum_ext", file=f2_hum_board_ext)
+        print("up_time,press_hpa", file=f1_press_hpa)
+        print("up_time,altitude_nn,press_hpa", file=f2_altitude_press)
+
         for line in f.readlines():
             if line.startswith("$"):
                 # every ``_`` is fairly uninteresting
-                _, up_time, utc, _, sats_in_use, lat, lon, _, speed_kmh, course, altitude, temp_board, temp_ext, hum_board, hum_ext, press_hpa, _, _ = \
+                _, up_time, utc, _, sats_in_use, lat, lon, _, speed_kmh, course, altitude_nn, temp_board, temp_ext, hum_board, hum_ext, press_hpa, _, _ = \
                     line.split(";")
+            else:
+                continue
+
+            print(p1.format(up_time, stats_in_use), file=f1_sats_in_use)
+            print(p1.format(up_time, lat), file=f1_lat)
+            print(p1.format(up_time, lon), file=f1_lon)
+            print(p2.format(up_time, lat, lon), file=f2_lat_lon)
+            print(p1.format(up_time, speed_kmh), file=f1_speed_kmh)
+            print(p1.format(up_time, course), file=f1_course)
+            print(p1.format(up_time, altitude_nn), file=f1_altitude_nn)
+            print(p1.format(up_time, temp_board), file=f1_temp_board)
+            print(p1.format(up_time, temp_ext), file=f1_temp_ext)
+            print(p2.format(up_time, temp_board, temp_ext), file=f2_temp_board_ext)
+            print(p1.format(up_time, hum_board), file=f1_hum_board)
+            print(p1.format(up_time, hum_ext), file=f1_hum_ext)
+            print(p2.format(up_time, hum_board, hum_ext), file=f2_hum_board_ext)
+            print(p1.format(up_time, press_hpa), file=f1_press_hpa)
+            print(p2.format(up_time, altitude_nn, press_hpa), file=f2_altitude_press)
